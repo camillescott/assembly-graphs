2b578cfd01add305e63124a6c024291dbbcc502c
Camille Scott <camille.scott.w@gmail.com>
2016-05-26 12:25:44 -0700

    motif correlation stuff, more glyph munging

--- motif_analysis.R
+++ motif_analysis.R
@@ -1,33 +1,20 @@
 library('yaml')
-library(digest)
-library(data.table)
-library(ggplot2)
-library(grid)
-library(gridGraphics)
-library(gridBase)
-library(gridExtra)
+library('digest')
+library('data.table')
+library('ggplot2')
+library('grid')
+library('gridGraphics')
+library('gridBase')
+library('gridExtra')
 library('igraph')
-library(Cairo)
-library("RColorBrewer")
-library(GGally)
-library(intergraph)
-
-data_dir = file.path('outputs/stats/31/invertebrate/')
-stats_files = dir(data_dir, pattern='*.stats', full.names = TRUE)
-stats_names = dir(data_dir, pattern='*.stats')
-
-dir.create('motifs')
-data = sapply(stats_files, FUN=function(d) { yaml.load_file(d)}, simplify = FALSE)
-names(data) <- sapply(strsplit(stats_names, split='[.]'), "[[", 1)
-
-kmer_files = dir(data_dir, pattern='*.uniquekmers', full.names = TRUE)
-kmer_names = sapply(strsplit(dir(data_dir, pattern='*.uniquekmers'), split='[.]'), "[[", 1)
-
-kmer.data = data.frame(genome=kmer_names,
-                       unique_kmers=as.integer(sapply(strsplit(sapply(kmer_files, function (f) {readLines(f)[1]}, USE.NAMES = FALSE), 
-                                                         split=' '),"[[", 1)))
-
-
+library('Cairo')
+library('RColorBrewer')
+library('GGally')
+library('intergraph')
+library('psych')
+library('reshape2')
+library('gplots')
+library('grImport')
 
 hash <- function(s) {
   digest(s, algo='xxhash32')
@@ -54,7 +41,7 @@ getMotifs <- function(name, data) {
 convertDot <- function(dot) {
   # this is dumb and really slow but it works
   dot = as.character(dot)
-
+  
   gmlfilename = file.path('motifs', paste(hash(dot), '.gml', sep=''))
   dotfilename = file.path('motifs', paste(hash(dot), '.dot', sep=''))
   if (!file.exists(gmlfilename)) {
@@ -65,12 +52,36 @@ convertDot <- function(dot) {
   read_graph(gmlfilename, format='gml')
 }
 
+#
+# Load data!
+#
+
+data_dir = file.path('outputs/stats/31/invertebrate/')
+stats_files = dir(data_dir, pattern='*.stats', full.names = TRUE)
+stats_names = dir(data_dir, pattern='*.stats')
+
+dir.create('motifs')
+data = sapply(stats_files, FUN=function(d) { yaml.load_file(d)}, simplify = FALSE)
+names(data) <- sapply(strsplit(stats_names, split='[.]'), "[[", 1)
+
+kmer_files = dir(data_dir, pattern='*.uniquekmers', full.names = TRUE)
+kmer_names = sapply(strsplit(dir(data_dir, pattern='*.uniquekmers'), split='[.]'), "[[", 1)
+
+kmer.data = data.frame(genome=kmer_names,
+                       unique_kmers=as.integer(sapply(strsplit(sapply(kmer_files, function (f) {readLines(f)[1]}, USE.NAMES = FALSE), 
+                                                         split=' '),"[[", 1)))
 
 motif.data = rbindlist(sapply(names(data), getMotifs, data, USE.NAMES = FALSE, simplify = FALSE))
 motif.data$graph <- sapply(motif.data$full_dot, convertDot, simplify = FALSE, USE.NAMES = FALSE)
 motif.data$iso_class <- as.factor(sapply(motif.data$graph, isomorphism_class, USE.NAMES = FALSE))
-
 all.data = merge(motif.data, kmer.data, by='genome')
+
+motif.graphs <- subset.data.frame(motif.data[ !duplicated(motif.data$iso_class)],
+                                  select=c('graph', 'motif_size', 'iso_class'))
+motif.colors <- brewer.pal(n = 8, name = 'Dark2')
+names(motif.colors) <- levels(motif.graphs$iso_class)
+motif.graphs$color <- motif.colors
+motif.graphs$filename <- sapply(motif.graphs$iso_class, function (item) { paste('Iso', item, '.ps', sep='')})
 
 p = ggplot(data = motif.data, aes(x=count))
 p + facet_grid(iso_class ~ motif_size, labeller = label_both) + 
@@ -85,11 +96,7 @@ ggplot(data = motif.data, aes(x=count, fill=iso_class)) +
   geom_density(alpha=0.3) +
   theme_bw()
 
-motif.graphs <- subset.data.frame(motif.data[ !duplicated(motif.data$iso_class)],
-                                  select=c('graph', 'motif_size', 'iso_class'))
-                       
-mycolors <- brewer.pal(n = 8, name = 'Dark2')
-names(mycolors) <- levels(motif.graphs$iso_class)
+
 
 plotMotifs <- function (data) {
   plot.new()
@@ -121,6 +128,79 @@ plotMotifs <- function (data) {
   a <- grid.grab(wrap=TRUE)
   a
 }
+
+saveMotifs <- function (data) {
+
+  for (i in 1:length(data$graph)) {
+    iso = data$iso_class[[i]]
+    g = data$graph[[i]]
+    V(g)$label = ''
+    E(g)$label = ''
+    filename = paste('Iso', iso, '.ps', sep='')
+    postscript(file=filename,horiz=TRUE,onefile=TRUE,width=2,height=2)
+    plot.igraph(g,
+                layout=layout_on_grid,
+                vertex.size = 30,
+                vertex.color=mycolors[[as.character(iso)]],
+                vertex.frame.color= "white",
+                vertex.label.color = "white",
+                vertex.label.family = "sans",
+                edge.width=1,
+                margin=c(1.0, 0, 1.0, 0),
+                edge.color="black",
+                asp=1)
+    #main=paste('Iso ', iso))
+    
+  }
+}
+
+saveMotif <- function(g, filename, node.color, main='') {
+  V(g)$label = ''
+  E(g)$label = ''
+  #postscript(file=filename,horiz=TRUE,onefile=TRUE,width=4,height=4)
+  dev.new()
+  plot.new()
+  par(mar=c(0,0,0,0))
+  plot.igraph(g,
+              layout=layout_on_grid,
+              vertex.size = 30,
+              vertex.color=node.color,
+              vertex.frame.color= "white",
+              vertex.label.color = "white",
+              vertex.label.family = "sans",
+              edge.width=1,
+              margin=c(1.0, 0, 1.0, 0),
+              edge.color="black",
+              asp=1,
+              main=main)
+
+  #dev.copy2eps(file=filename, width=4)
+  dev.print(file=filename, width=4)
+
+}
+
+for (i in 1:nrow(motif.graphs)) {
+  saveMotif(motif.graphs[[i, 'graph']], 
+            motif.graphs[[i, 'filename']], 
+            motif.graphs[[i, 'color']])
+}
+
+motif.glyphs <- sapply(motif.graphs$filename, function(fn) {
+  out_fn <- paste(fn, '.xml', sep='')
+  PostScriptTrace(fn, out_fn)
+  readPicture(out_fn)
+})
+names(motif.glyphs) <- sapply(motif.graphs$iso_class, function (n) { paste('Iso', n)})
+
+motif.glyph.axis <- function () {
+    function(label, x=0.5, y=0.5, ...) {
+      print(label)
+      ggplot2:::absoluteGrob(
+        do.call("gList", mapply(symbolsGrob, motif.glyphs[label], x, y, SIMPLIFY = FALSE)),
+        height = unit(1.5, "cm"))
+    }
+}
+
 motifs.grob <- plotMotifs(motif.graphs)
 
 ggplot(data = all.data, aes(x=unique_kmers, y=count)) +
@@ -156,3 +236,36 @@ grid.arrange(grobs=lapply(motif.graphs$graph,
                           USE.NAMES = FALSE), 
              nrow=4, ncol=2, widths=c(1, 1), heights=rep(1, 4))
 
+reorder_cormat <- function(cormat){
+  # Use correlation between variables as distance
+  dd <- as.dist((1-cormat)/2)
+  hc <- hclust(dd)
+  cormat <-cormat[hc$order, hc$order]
+  cormat
+}
+
+all.melted <- melt(all.data, id=c('genome', 'iso_class'), measure.vars=c('count'))
+motif.data <- as.data.frame(cast(all.melted, genome ~ iso_class, fill=0)[2:9])
+names(motif.data) <- sapply(names(motif.data), function (n) { paste('Iso', n)})
+names(motif.data) <- as.factor(names(motif.data))
+motif.corr <- reorder_cormat(round(cor(motif.data), 3))
+
+
+g <- ggplot(melt(motif.corr), aes(X1, X2, fill = value)) +
+  geom_tile(color = "white") +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal() +
+  coord_fixed() +
+  scale_x_discrete(labels=motif.glyph.axis())
+print(ggheatmap)
+
+grid.arrange(plotMotifs(motif.graphs),
+             ggcorr(motif.data, nbreaks = 10),
+             widths=c(3, 12),
+             heights=c(3, 3, 3),
+             layout_matrix=rbind(c(NA,2), 
+                                 c(1,2),
+                                 c(NA,2)))
+ggcorr(motif.data, nbreaks = 10)
